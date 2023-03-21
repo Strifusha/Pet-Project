33aa39a2d2a3442fd6d5691c081cc085d4572868
oleksii.yefanov <oleksii.yefanov@pm.bet>
2023-03-21 20:47:08 +0200

    feat: initial project

---
+++ js/main.js
@@ -0,0 +1,33 @@
+// 1. Найти кнопку
+// 2. навесить обработчик события
+// 3. Получить данные с формы
+//     3.1 Проверка на пустоту имя и комметарий
+//    3.2 Добавить дату публикации
+// 4. Записат в масив комментариев
+// 5. Вывести на страницу
+//     5.1 Сортировка,
+
+
+// var comments = [{
+//     useName: '',
+//     comment: 'dsfsdf',
+//     date: '23.03.2023'
+// }, {
+//     useName: '',
+//     comment: 'dsfsdf',
+//     date: '23.03.2023'
+// }, {
+//     useName: '',
+//     comment: 'dsfsdf',
+//     date: '23.03.2023'
+// }]
+
+
+function saveComments() {
+    // const userName = document.
+}
+
+const saveBtn = document.getElementById('comment-save');
+
+saveBtn.addEventListener('click', saveComments)
+
