f38809b26dbdeb1a1dbf297020151b6278d4c860
Strifusha <KatushaStrifusha@gmail.com>
2023-04-03 23:07:27 +0300

    refuctoring loop of value + css added

--- js/main.js
+++ js/main.js
@@ -8,90 +8,65 @@
 // 5. Вывести на страницу
 //     5.1 Сортировка,
 
-
-// var comments = [{
-//     useName: '',
-//     comment: 'dsfsdf',
-//     date: '23.03.2023'
-// }, {
-//     useName: '',
-//     comment: 'dsfsdf',
-//     date: '23.03.2023'
-// }]
-
 let comments = [];
 
+
 function saveComments() {
+    const allFields = document.getElementsByClassName('field');
+    
+    let userName;
+    let userSurname;
+    let userComment; 
 
-    const userName = document.getElementById('user-name');
-    const userSurname = document.getElementById('user-surname');
-    const commentText = document.getElementById('comment-text');
+    let showErrors = () =>{
+        for (let i = 0; i < allFields.length; i++) {
+            if(allFields[i].value == '') {
+                allFields[i].nextElementSibling.classList.add('show-error');
+                allFields[i].value = ''; 
+              } else {   
+                userName = allFields[0].value;
+                userSurname = allFields[1].value;
+                userComment = allFields[2].value;
+                allFields[i].nextElementSibling.classList.remove('show-error');
+                 }        
+            };                 
+        }  
+    showErrors();
+ 
 
-    const userNameValue = userName.value;
-    const userSurnameValue = userSurname.value
-    const commentTextValue = commentText.value;
+    const isHasError =  document.querySelectorAll('.show-error');
+    if (isHasError.length) return;
 
-    const isHasError = checkErrors(userNameValue, userSurnameValue, commentTextValue);
-    if (isHasError) return;
-
-    const newComment = createComment(userNameValue, userSurnameValue, commentTextValue);
-
-    comments.push(newComment);
-    userName.value = '';
-    userSurname.value = '';
-    commentText.value = '';
-
-    renderComments();
-}
-
-const createComment = (useName, useSurname, comment) => {
-    return {
-        'useName': useName.trim(),
-        'useSurname' : useSurname.trim(),
-        'comment': comment.trim(),
-        date: new Date(),
-        }
-}
-
-const renderComments = () => {
-
-    const getCommentSection = document.getElementById('show-comments');
-
-    let allComments = '';
-
-    for(let i = 0; i < comments.length; i++) {
-
-        allComments += `<article>
-                    <h4>${comments[i].useName} ${comments[i].useSurname}</h4>
-                    <span>${comments[i].date}</span>
-                    <div>${comments[i].comment}</div>
-                </article>`;
+    const createComment = () => {
+        return {
+            'usName': userName.trim(),
+            'surname' : userSurname.trim(),
+            'comment': userComment.trim(),
+            date: new Date(),
+        }   
     }
+
+    const newComment = createComment(userName, userSurname, userComment)
+    comments.push(newComment);
+    //console.log(comments);
+
+
+    const showComment = () => {
+        const getCommentSection = document.getElementById('show-comments');
+            let allComments = '';
+            for(let i = 0; i < comments.length; i++) {
+            allComments += `<article>
+                            <h3>${comments[i].usName} ${comments[i].surname}</h3>
+                            <span>${comments[i].date}</span>
+                            <div class='div-comment'>${comments[i].comment}</div>
+                            </article>`;
+        }       
+        getCommentSection.innerHTML = allComments;     
+    }
+    showComment();
+
+}   
+
+    const saveBtn = document.getElementById('comment-save');
+    saveBtn.addEventListener('click', saveComments);
     
-    getCommentSection.innerHTML = allComments;
-
-}
-
-const checkErrors = (userNameValue, userSurnameValue, commentTextValue) => {
-    
-    let showErr = document.querySelectorAll('.error');
-    
-          showErr.forEach(function(showErr, index) {
-
-            showErr.classList.remove('show-error');
-            
-            if (userNameValue.length == 0 && index === 0) {
-                showErr.classList.add('show-error');
-            }
-            if (userSurnameValue.length == 0 && index === 1) {
-                showErr.classList.add('show-error');
-            }
-            if (commentTextValue.length == 0 && index === 2) {
-                showErr.classList.add('show-error');
-            }
-            return
-        })
- };
-
-const saveBtn = document.getElementById('comment-save');
-saveBtn.addEventListener('click', saveComments);
